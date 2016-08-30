b5c727e339702aeca00f27d415665eb7b27a7573
Rahul Kumar Singh <kumarahul120292@gmail.com>
2016-08-30 23:52:45 +0530

    Create KMP_String_Matching.java

---
+++ String/KMP_String_Matching.java
@@ -0,0 +1,62 @@
+package Algorithms;
+
+/**
+ * Created by rahul.sk on 30/08/16.
+ */
+public class KMP_String_Matching {
+
+    void KMPSearch(String pat,String txt){
+        int M=pat.length();
+        int N=txt.length();
+        int lps[]=new int[M];
+        int j=0;
+        computeLPSArray(pat,M,lps);
+        int i=0;
+        while(i<N){
+            if(pat.charAt(j)==txt.charAt(i)){
+                i++;
+                j++;
+            }
+            if (j==M){
+                System.out.println("Found pattern at index " + (i-j));
+                j=lps[j-1];
+            }
+            else if(i < N && pat.charAt(j) != txt.charAt(i)){
+                if (j!=0){
+                    j=lps[j-1];
+                }
+                else{
+                    i=i+1;
+                }
+
+            }
+        }
+    }
+    void computeLPSArray(String pat,int M,int lps[]){
+        int len=0;
+        int i=1;
+        lps[0]=0;
+        while (i<M){
+            if(pat.charAt(i)==pat.charAt(len)){
+                len++;
+                lps[len]=len;
+                i++;
+            }
+            else{
+                if (len != 0){
+                    len=lps[len-1];
+                }
+                else{
+                    lps[i]=len;
+                    i++;
+                }
+            }
+        }
+
+
+    }
+    public static void main(String[] args) {
+        String txt="AJHSDJHSJDBFCAJHBHJBHJADBJA";
+        String pat="AJASDJ";
+    }
+}
