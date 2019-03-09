7ef74cd660dfdc4b754aadbc63cd2a1e2ef54c70
Rahul Kumar Singh <kumarahul120292@gmail.com>
2019-03-09 17:48:30 +0530

    Create ClimbingStairs.java

---
+++ LeetCode/ClimbingStairs.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/climbing-stairs/
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp=new int[n+1];
+        if(n<=2)
+            return n; 
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++)
+            dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
+    }
+}
